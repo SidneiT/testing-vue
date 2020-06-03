c608ce667c9a7b387c77e53472e5b22248b87aaa
Sidnei Teixeira <sidnei.teixeira@outlook.com>
2020-06-03 01:09:02 -0300

    ToContains vs toBe,

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,10 +1,17 @@
+/* eslint-disable no-debugger */
 import Item from '../Item.vue'
 import { mount, shallowMount } from '@vue/test-utils'
 
 describe('Item.vue', () => {
-  test('sanity test', () => {
-    const wrapper = shallowMount(Item)
-    debugger
-    return expect(wrapper.text()).toContain('item')
+  test('render items.url', () => {
+    const item = {
+      url: 10
+    }
+
+    const wrapper = shallowMount(Item, {
+      propsData: { item }
+    })
+
+    return expect(wrapper.text()).toContain(item.url)
   })
 })
